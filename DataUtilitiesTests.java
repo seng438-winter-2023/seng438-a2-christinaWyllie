f3233887cc6f9a03ef6331ebb85a43e62a3197f2
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2023-02-09 08:45:23 -0700

    Update DataUtilitiesTests.java
    Added Comments and more tests

--- DataUtilitiesTests.java
+++ DataUtilitiesTests.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.jfree.data.DataUtilitiesTests;
 
 import static org.junit.Assert.*;
@@ -14,6 +11,7 @@ import java.security.InvalidParameterException;
  * @author sobia
  *
  */
+
 public class DataUtilitiesTests {
 
 	private Number[] myNumberArray;
@@ -43,10 +41,10 @@ public class DataUtilitiesTests {
 	@Before
 	public void setUp() throws Exception 
 	{
-		double[] myDoubleArray = {1.0, 1, 2.01, 5};
+		double[] myDoubleArray = {-12356015.0, 1, 2.01, 50000000};
 		myNumberArray = DataUtilities.createNumberArray(myDoubleArray);
 		
-		double[][] my2DDoubleArray = {{1.0}, {1, 2.01}, {5}};
+		double[][] my2DDoubleArray = {{-12356015.0}, {1, 2.01}, {50000000}};
 		myNumberArray2D = DataUtilities.createNumberArray2D(my2DDoubleArray);
 	}
 
@@ -54,65 +52,206 @@ public class DataUtilitiesTests {
 	 * @throws java.lang.Exception
 	 */
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception 
+	{
+		
 	}
-
+	
+//CreateNumberArray Tests 
+	
+	/* Equivalence Class Test: Test that valid double[] input creates correct
+	 * Double[] output.
+	 */
+	
+	@Test
+	public void createNumberArrayValidInputTest() 
+	{
+		Double[] expectedDoubleArray = {-12356015.0, 1.0, 2.01, 50000000.0};
+		assertArrayEquals(expectedDoubleArray, myNumberArray);
+	}
+	
+	/* Equivalence Class Test: Test that valid double[] input creates correct
+	 * length Double[] output.
+	 */
+	
 	@Test
 	public void createNumberArrayValidInputLengthTest() 
 	{
-		Double[] expectedDoubleArray = {1.0, 1.0, 2.01, 5.0};
+		Double[] expectedDoubleArray = {-12356015.0, 1.0, 2.01, 50000000.0};
 		assertEquals(expectedDoubleArray.length, myNumberArray.length);
 	}
 	
-	@Test (expected = InvalidParameterException.class)
-	public void createNumberArrayInvalidCharInputTest() throws InvalidParameterException
+	/* Boundary Value Test: When given max value that can be held in a double
+	 * correct Number array is created
+	 */
+	
+	@Test
+	public void createNumberArrayLargeInputTest() 
 	{
-		double[] myCharArray = {};
-		myNumberArray = DataUtilities.createNumberArray(myCharArray);
+		Double[] expectedDoubleArray = {Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE};
+		double[] inputArray = {Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE};
+		myNumberArray = DataUtilities.createNumberArray(inputArray);
+		assertArrayEquals(expectedDoubleArray, myNumberArray);
 	}
 	
-//	@Test
-//	public void createNumberArrayThrowsException() {
-//		fail("Not yet implemented");
-//	}
-//	
-//	@Test
-//	public void createNumberArray2DInvalidInputTest() {
-//		fail("Not yet implemented");
-//	}
-//	
-	//Test that putting in a valid 2D double array creates the expected Double Array
+	/* Boundary Value Test: When given the largest negative number that can be held in a double
+	 * correct number array is created 
+	 */
+	
+	@Test
+	public void createNumberArraySmallInputTest() 
+	{
+		Double[] expectedDoubleArray = {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+		double[] inputArray = {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+		myNumberArray = DataUtilities.createNumberArray(inputArray);
+		assertArrayEquals(expectedDoubleArray, myNumberArray);
+	}
+	
+	/* Equivalence Class Test: Pass in empty double array as input and test that 
+	 * empty Double array created 
+	 */
+	
+	@Test
+	public void createNumberArrayEmptyInputTest()
+	{
+		double[] emptyArray = {};
+		Double[] expectedDoubleArray = {};
+		myNumberArray = DataUtilities.createNumberArray(emptyArray);
+		assertArrayEquals(expectedDoubleArray, myNumberArray);
+	}
+	
+	/* Boundary Value Test: Test that when null passed in as argument, 
+	 * InvalidParameterException is thrown
+	 * Catch InvalidParameterExeption if thrown and assert that test has passed
+	 * Catch all other exceptions and assert that test failed. 
+	 * 
+	 * Cannot pass in char[], int[], Double[] etc. Gives compilation errors
+	 */
+	
+	@Test //(expected = InvalidParameterException.class)
+	public void createNumberArrayThrowsInvalidParameterExceptionTest() throws InvalidParameterException
+	{	
+		try 
+		{
+			double[] invalidArray = null;
+			myNumberArray = DataUtilities.createNumberArray(invalidArray); // throws illegal argument exception when invalidArray = null
+		}
+		
+		catch (InvalidParameterException e)
+		{
+			assertTrue("InvalidParameterException correctly thrown for invalid input null", true);
+		}
+		
+		catch (Exception e)
+		{
+			fail();
+		}
+
+	}
+
+//CreateNumberArray2D Tests
+	
+	/* Equivalence Class Test: Test that passing in a valid 2D double array creates 
+	 * the expected Double Array
+	 */
+	
 	@Test
 	public void createNumberArray2DValidInputTest() 
 	{
-		Double[][] expected2DDoubleArray = {{1.0}, {1.0, 2.01}, {5.0}};
+		Double[][] expected2DDoubleArray = {{-12356015.0}, {1.0, 2.01}, {50000000.0}};
 		assertArrayEquals(expected2DDoubleArray, myNumberArray2D);
 	}
 	
-	// Test that array created has the correct number of rows
+	/* Equivalence Class Test: Test that Double[] array created has the correct 
+	 * number of rows
+	 */
+	
 	@Test
 	public void createNumberArray2DCorrectRowsTest() 
 	{
-		Double[][] expected2DDoubleArray = {{1.0}, {1.0, 2.01}, {5.0}};
+		Double[][] expected2DDoubleArray = {{-12356015.0}, {1.0, 2.01}, {50000000.0}};
 		assertEquals(expected2DDoubleArray.length, myNumberArray2D.length);
 	}
 	
-	// Test that array created has the correct number of columns
+	/* Equivalence Class Test: Test that array created has the correct 
+	 * number of columns for each row
+	 */
+	
 	@Test
 	public void createNumberArray2DCorrectColumnsTest() 
 	{
-		Double[][] expected2DDoubleArray = {{1.0}, {1.0, 2.01}, {5.0}};
+		Double[][] expected2DDoubleArray = {{-12356015.0}, {1.0, 2.01}, {50000000.0}};
 		for(int i = 0; i < expected2DDoubleArray.length; i++)
 		{
 			assertEquals(expected2DDoubleArray[i].length, myNumberArray2D[i].length);
+		}	
+	}
+	
+	/* Equivalence Class Test: Pass in empty double[][] as input and test that 
+	 * empty Double array created 
+	 */
+	
+	@Test
+	public void createNumberArray2DEmptyInputTest()
+	{
+		double[][] empty2DArray = {{}, {}, {}};
+		Double[][] expected2DDoubleArray = {{}, {}, {}};
+		myNumberArray2D = DataUtilities.createNumberArray2D(empty2DArray);
+		assertArrayEquals(expected2DDoubleArray, myNumberArray2D);
+	}
+
+	/* Boundary Value Test: When given max value that can be held in a double
+	 * correct Number array is created
+	 */
+	
+	@Test
+	public void createNumberArray2DLargeInputTest() 
+	{
+		Double[][] expected2DDoubleArray = {{Double.MAX_VALUE, Double.MAX_VALUE}, {Double.MAX_VALUE}, {Double.MAX_VALUE}};
+		double[][] inputArray = {{Double.MAX_VALUE, Double.MAX_VALUE}, {Double.MAX_VALUE}, {Double.MAX_VALUE}};
+		myNumberArray2D = DataUtilities.createNumberArray2D(inputArray);
+		assertArrayEquals(expected2DDoubleArray, myNumberArray2D);
+	}
+	
+	/* Boundary Value Test: When given the largest negative number that can be held in a double
+	 * correct number array is created 
+	 */
+	
+	@Test
+	public void createNumberArray2DSmallInputTest() 
+	{
+		Double[][] expected2DDoubleArray = {{Double.MIN_VALUE, Double.MIN_VALUE}, {Double.MIN_VALUE}, {Double.MIN_VALUE}};
+		double[][] inputArray = {{Double.MIN_VALUE, Double.MIN_VALUE}, {Double.MIN_VALUE}, {Double.MIN_VALUE}};
+		myNumberArray2D = DataUtilities.createNumberArray2D(inputArray);
+		assertArrayEquals(expected2DDoubleArray, myNumberArray2D);
+	}
+	
+	/* Boundary Value Test: Test that when null passed in as argument, 
+	 * InvalidParameterException is thrown
+	 * Catch InvalidParameterExeption if thrown and assert that test has passed
+	 * Catch all other exceptions and assert that test failed. 
+	 * 
+	 * Cannot pass in char[][], int[][], Double[][], etc. Gives compilation errors
+	 */
+	
+	@Test //(expected = InvalidParameterException.class)
+	public void createNumberArray2DThrowsExceptionTest() 
+	{
+		try
+		{
+			double[][] invalidArray = null;
+			myNumberArray2D = DataUtilities.createNumberArray2D(invalidArray);
 		}
 		
+		catch (InvalidParameterException e)
+		{
+			assertTrue(true);
+		}
+		
+		catch (Exception e)
+		{
+			fail();
+		}
 	}
-//	
-//	@Test
-//	public void createNumberArray2DThrowsExceptionTest() {
-//		fail("Not yet implemented");
-//	}
 
 }
-
