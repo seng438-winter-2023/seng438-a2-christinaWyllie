3aa479ae7315b77fb29a5cf82b4a23fdc78e29f5
MaitryRohit <101202431+MaitryRohit@users.noreply.github.com>
2023-02-10 11:45:04 -0700

    Update IntersectsRangeTest.java

--- IntersectsRangeTest.java
+++ IntersectsRangeTest.java
@@ -22,30 +22,37 @@ public class IntersectsRangeTest {
     //Will be used for every test specified below
     @Before
     public void setUp() throws Exception { 
-    	testRange = new Range(-5, 5);
+    	testRange = new Range(-5.0, 5.0);
     }
     
-    /* Test: intersectExceptionThrowInvalidLowerBound
-     * Description: Throws an exception when given a lower bound that is greater 
-     * than the upper bound
+    /* Test: intersectInvalidLowerBound
+     * Description: Sends a lower bound that is greater than the upper bound 
+     * 
+     * Assumption: An assumption is made that this data will intersect 
+     * because if the bounds were switched it would return true
      */
 
-    @Test (expected = Exception.class)  
-    public void intersectExceptionThrowInvalidLowerBound() throws Exception {
-    	testRange.intersects(17, 4.75);
+    @Test  
+    public void intersectInvalidLowerBound() {
+    	assertTrue("Given range should intersect with initialized range, but does not.",
+    	testRange.intersects(17, 4.75));
     }
     
-    /* Test: intersectExceptionThrowInvalidUpperBound
-     * Description: Throws an exception when given an upper bound that is less 
-     * than the lower bound
+    /* Test: intersectInvalidUpperBound
+     * Description: Sends a upper bound that is greater than the lower bound 
+     * 
+     * Assumption: An assumption is made that this data will intersect 
+     * because if the bounds were switched it would return true
      */
-    @Test(expected = Exception.class) 
-    public void intersectExceptionThrowInvalidUpperBound() throws Exception {
-        testRange.intersects(4, -3);
+    @Test
+    public void intersectInvalidUpperBound(){
+    	assertTrue("Given range should intersect with initialized range, but does not.",
+    	testRange.intersects(7.0, -3.0));
     }
     
     
-    /* Test: intersectWithinRange
+    /* Note: Equivalence Test
+     * Test: intersectWithinRange
      * Description: When given bound that is within the mock range the test passes
      * Expected Return: True
      */
@@ -55,7 +62,8 @@ public class IntersectsRangeTest {
     			testRange.intersects(-4.99, 4.99));
     }
     
-    /* Test: intersectIsRange
+    /* Note: Boundary Test
+     * Test: intersectIsRange
      * Description: Gives an intersect range equal to the mock range
      * Expected Return: True
      */
@@ -65,7 +73,8 @@ public class IntersectsRangeTest {
     			testRange.intersects(-5, 5));
     }
     
-    /* Test: intersectOverlappingLowerBound
+    /* Note: Equivalence Test
+     * Test: intersectOverlappingLowerBound
      * Description: Lower bound given is within the range but the upper bound is not
      * Expected Return: True
      */
@@ -76,7 +85,8 @@ public class IntersectsRangeTest {
     }
     
     
-    /* Test: intersectOverlappingUpperBound
+    /* Note: Equivalence Test
+     * Test: intersectOverlappingUpperBound
      * Description: Upper bound given is within the range but the lower bound is not
      * Expected Return: True
      */
@@ -86,24 +96,26 @@ public class IntersectsRangeTest {
     			testRange.intersects(-10, -4.75));
     }  
     
-    /* Test: intersectNoOverlappingBoundLHS
+    /* Note: Equivalence Test
+     * Test: intersectNoOverlappingBoundLHS
      * Description: Neither the upper or lower bound should overlap with the 
      * given range on the left hand side of range 
      * Expected Return: False
      */
     @Test
-    public void intersectNoOverlappingBoundLHS() {
+    public void NoOverlappingBoundLHS() {
         assertFalse("Given range should not intersect with initialized range, but does.",
         testRange.intersects(-10, -5.01));
     }  
     
-    /* Test: intersectNoOverlappingBoundRHS
+    /* Note: Equivalence Test
+     * Test: intersectNoOverlappingBoundRHS
      * Description: Neither the upper or lower bound should overlap with the 
      * given range on the right hand side range
      * Expected Return: False
      */
     @Test
-    public void intersectNoOverlappingBoundRHS() {
+    public void NoOverlappingBoundRHS() {
         assertFalse("Given range should not intersect with initialized range, but does.",
         testRange.intersects(5.01, 10));
     }  
